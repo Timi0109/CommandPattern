d03f51218d594aae051211bd8261d0efb9fcaa48
Merphous Liu <merphousliu@gmail.com>
2021-07-28 20:27:06 -0400

    //changed api name

---
+++ 2020_12_Command_Pattern/src/Divide.java
@@ -0,0 +1,18 @@
+
+public class Divide implements Operations {
+	private int a;
+	private int b;
+
+	public Divide(int a, int b) {
+		// TODO Auto-generated constructor stub
+		this.a = a;
+		this.b = b;
+	}
+
+	@Override
+	public int exexecute() {
+		// TODO Auto-generated method stub
+		return a / b;
+	}
+
+}
---
+++ 2020_12_Command_Pattern/src/Operations.java
@@ -0,0 +1,4 @@
+
+public interface Operations {
+	int exexecute();
+}
