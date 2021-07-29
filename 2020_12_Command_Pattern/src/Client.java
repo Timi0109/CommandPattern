122e5a0db0728d98fce5be99cc3326febeb4a1cd
001083544_yanlinliu <253939101@qq.com>
2021-07-28 17:36:01 -0700

    add client class

---
+++ 2020_12_Command_Pattern/src/Client.java
@@ -0,0 +1,24 @@
+
+public class Client {
+	
+	public Client() {
+		
+	}
+	
+	public void add() {
+		
+	}
+	
+	public void sub() {
+		
+	}
+	
+	public void divide() {
+		
+	}
+	
+	
+	public void multiply() {
+		
+	}
+}
